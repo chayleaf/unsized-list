423acca55795a8f901cfa02a0a687caf65e655e6
chayleaf <chayleaf-git@pavluk.org>
2023-04-12 21:32:32 +0700

    no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 //! This was done purely for fun and learning
 //! Tested with miri, so there aren't any surface-level bugs
-use std::{marker::PhantomData, mem::MaybeUninit, ops::{Deref, DerefMut}, path::Path, ffi::{OsStr, CStr}, os::unix::prelude::OsStrExt};
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
+use core::{marker::PhantomData, mem::{self, MaybeUninit}, ops::{Deref, DerefMut}, ptr, slice};
+use core::ffi::CStr;
+
+#[cfg(feature = "std")]
+use std::{path::Path, ffi::OsStr};
+#[cfg(all(feature = "std", unix))]
+use std::os::unix::prelude::OsStrExt;
+
+#[cfg(not(feature = "std"))]
+use alloc::alloc as alloc2;
+#[cfg(feature = "std")]
+use std::alloc as alloc2;
 
 pub struct ListSlice<T: ?Sized> {
     align: usize,
@@ -21,12 +37,12 @@ pub struct ListSliceTailMut<'a, T: ?Sized> {
 
 impl<T: ?Sized + CopyableUnsized> ListSlice<T> {
     pub fn head(&self) -> Option<&T> {
-        let data = unsafe { std::slice::from_raw_parts(self.chunks, self.chunks_len) };
+        let data = unsafe { slice::from_raw_parts(self.chunks, self.chunks_len) };
         let len = usize::from_ne_bytes(
-            data.get(0..std::mem::size_of::<usize>())
+            data.get(0..mem::size_of::<usize>())
                 .and_then(|x| TryFrom::try_from(x).ok())?,
         );
-        data.get(std::mem::size_of::<usize>()..std::mem::size_of::<usize>() + len)
+        data.get(mem::size_of::<usize>()..mem::size_of::<usize>() + len)
             .map(|x| unsafe {
                 T::construct_ptr(x.as_ptr(), len)
             })
@@ -49,14 +65,14 @@ impl<T: ?Sized> ListSlice<T> {
     /// # Safety
     /// a fat pointer must be constructible from a pointer and a byte count divided by n
     pub unsafe fn head_unsafe(&self, n: usize) -> Option<&T> {
-        let data = std::slice::from_raw_parts(self.chunks, self.chunks_len);
+        let data = slice::from_raw_parts(self.chunks, self.chunks_len);
         let len = usize::from_ne_bytes(
-            data.get(0..std::mem::size_of::<usize>())
+            data.get(0..mem::size_of::<usize>())
                 .and_then(|x| TryFrom::try_from(x).ok())?,
         );
-        data.get(std::mem::size_of::<usize>()..std::mem::size_of::<usize>() + len)
+        data.get(mem::size_of::<usize>()..mem::size_of::<usize>() + len)
             .map(|x| unsafe {
-                *std::mem::transmute::<&(*const u8, usize), &&T>(&(x.as_ptr(), len / n))
+                *mem::transmute::<&(*const u8, usize), &&T>(&(x.as_ptr(), len / n))
             })
     }
     /// # Safety
@@ -66,10 +82,10 @@ impl<T: ?Sized> ListSlice<T> {
         self.head_unsafe(n).map(|x| unsafe { &mut *(x as *const T as *mut T) })
     }
     pub fn tail(&self) -> Option<ListSliceTail<T>> {
-        let data = unsafe { std::slice::from_raw_parts(self.chunks, self.chunks_len) };
-        let len = std::mem::size_of::<usize>()
+        let data = unsafe { slice::from_raw_parts(self.chunks, self.chunks_len) };
+        let len = mem::size_of::<usize>()
             + usize::from_ne_bytes(
-                data.get(0..std::mem::size_of::<usize>())
+                data.get(0..mem::size_of::<usize>())
                     .and_then(|x| TryFrom::try_from(x).ok())?,
             );
         let aligned_len = if len % self.align == 0 {
@@ -122,8 +138,8 @@ pub struct List<T: ?Sized> {
     chunks_cap: usize,
 }
 
-impl<T: ?Sized> std::fmt::Debug for ListSlice<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl<T: ?Sized> core::fmt::Debug for ListSlice<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.write_str("ListSlice(&[ ")?;
         for i in 0..self.chunks_len {
             write!(f, "{} ", unsafe { *self.chunks.add(i) })?;
@@ -132,8 +148,8 @@ impl<T: ?Sized> std::fmt::Debug for ListSlice<T> {
     }
 }
 
-impl<T: ?Sized> std::fmt::Debug for List<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl<T: ?Sized> core::fmt::Debug for List<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.write_str("List(&[ ")?;
         for i in 0..self.inner.chunks_len {
             write!(f, "{} ", unsafe { *self.inner.chunks.add(i) })?;
@@ -154,39 +170,39 @@ pub unsafe trait CopyableUnsized {
 }
 unsafe impl<T: Copy> CopyableUnsized for [T] {
     unsafe fn construct_ptr<'a>(data: *const u8, len: usize) -> &'a Self {
-        debug_assert_eq!(len % std::mem::size_of::<T>(), 0);
-        std::slice::from_raw_parts(data as *const T, len / std::mem::size_of::<T>())
+        debug_assert_eq!(len % mem::size_of::<T>(), 0);
+        slice::from_raw_parts(data as *const T, len / mem::size_of::<T>())
     }
 }
 unsafe impl CopyableUnsized for str {
     unsafe fn construct_ptr<'a>(data: *const u8, len: usize) -> &'a Self {
-        std::str::from_utf8_unchecked(std::slice::from_raw_parts(data, len))
+        core::str::from_utf8_unchecked(slice::from_raw_parts(data, len))
     }
 }
 
-#[cfg(unix)]
+#[cfg(all(feature = "std", unix))]
 unsafe impl CopyableUnsized for OsStr {
     unsafe fn construct_ptr<'a>(data: *const u8, len: usize) -> &'a Self {
-        OsStr::from_bytes(std::slice::from_raw_parts(data, len))
+        OsStr::from_bytes(slice::from_raw_parts(data, len))
     }
 }
 
-#[cfg(unix)]
+#[cfg(all(feature = "std", unix))]
 unsafe impl CopyableUnsized for Path {
     unsafe fn construct_ptr<'a>(data: *const u8, len: usize) -> &'a Self {
-        Path::new(OsStr::from_bytes(std::slice::from_raw_parts(data, len)))
+        Path::new(OsStr::from_bytes(slice::from_raw_parts(data, len)))
     }
 }
 
 unsafe impl CopyableUnsized for CStr {
     unsafe fn construct_ptr<'a>(data: *const u8, len: usize) -> &'a Self {
-        CStr::from_bytes_with_nul_unchecked(std::slice::from_raw_parts(data, len))
+        CStr::from_bytes_with_nul_unchecked(slice::from_raw_parts(data, len))
     }
 }
 
 unsafe impl<T: Copy> CopyableUnsized for T {
     unsafe fn construct_ptr<'a>(data: *const u8, len: usize) -> &'a Self {
-        debug_assert_eq!(len, std::mem::size_of::<T>());
+        debug_assert_eq!(len, mem::size_of::<T>());
         &*(data as *const Self)
     }
 }
@@ -208,8 +224,8 @@ impl<T: ?Sized> List<T> {
     pub unsafe fn new_unsafe() -> Self {
         Self {
             inner: ListSlice {
-                align: std::mem::align_of::<usize>(),
-                chunks: std::ptr::null_mut(),
+                align: mem::align_of::<usize>(),
+                chunks: ptr::null_mut(),
                 chunks_len: 0,
                 _ph: PhantomData::default(),
             },
@@ -217,11 +233,11 @@ impl<T: ?Sized> List<T> {
         }
     }
     pub fn push(&mut self, data: &T) {
-        let len = std::mem::size_of_val(data);
-        let align = std::mem::align_of_val(data);
+        let len = mem::size_of_val(data);
+        let align = mem::align_of_val(data);
         let old_align = self.inner.align;
         self.inner.align = self.inner.align.max(align);
-        let usize_len = std::mem::size_of::<usize>().max(align);
+        let usize_len = mem::size_of::<usize>().max(align);
         let total_len = usize_len + len;
         let aligned_len = if self.inner.chunks_len % self.inner.align == 0 {
             self.inner.chunks_len
@@ -231,16 +247,16 @@ impl<T: ?Sized> List<T> {
         if old_align != self.inner.align || total_len + aligned_len > self.chunks_cap {
             if self.inner.chunks.is_null() {
                 self.inner.chunks = unsafe {
-                    std::alloc::alloc(
-                        std::alloc::Layout::from_size_align(total_len * 2, self.inner.align).unwrap(),
+                    alloc2::alloc(
+                        alloc2::Layout::from_size_align(total_len * 2, self.inner.align).unwrap(),
                     )
                 };
                 self.chunks_cap = total_len * 2;
             } else if old_align == self.inner.align {
                 self.inner.chunks = unsafe {
-                    std::alloc::realloc(
+                    alloc2::realloc(
                         self.inner.chunks,
-                        std::alloc::Layout::from_size_align(self.chunks_cap, self.inner.align).unwrap(),
+                        alloc2::Layout::from_size_align(self.chunks_cap, self.inner.align).unwrap(),
                         (aligned_len + total_len) * 2,
                     )
                 };
@@ -248,24 +264,24 @@ impl<T: ?Sized> List<T> {
             } else {
                 unsafe {
                     let old_chunks = self.inner.chunks;
-                    self.inner.chunks = std::alloc::alloc(
-                        std::alloc::Layout::from_size_align(
+                    self.inner.chunks = alloc2::alloc(
+                        alloc2::Layout::from_size_align(
                             (aligned_len + total_len) * 2,
                             self.inner.align,
                         )
                         .unwrap(),
                     );
-                    std::slice::from_raw_parts_mut(
+                    slice::from_raw_parts_mut(
                         self.inner.chunks as *mut MaybeUninit<u8>,
                         self.inner.chunks_len,
                     )
-                    .copy_from_slice(std::slice::from_raw_parts(
+                    .copy_from_slice(slice::from_raw_parts(
                         old_chunks as *const MaybeUninit<u8>,
                         self.inner.chunks_len,
                     ));
-                    std::alloc::dealloc(
+                    alloc2::dealloc(
                         old_chunks,
-                        std::alloc::Layout::from_size_align(self.chunks_cap, old_align).unwrap(),
+                        alloc2::Layout::from_size_align(self.chunks_cap, old_align).unwrap(),
                     );
                     self.chunks_cap = (aligned_len + total_len) * 2;
                 }
@@ -273,11 +289,11 @@ impl<T: ?Sized> List<T> {
         }
         unsafe {
             (self.inner.chunks.add(aligned_len) as *mut [u8; 8]).write(len.to_ne_bytes());
-            std::slice::from_raw_parts_mut(
+            slice::from_raw_parts_mut(
                 self.inner.chunks.add(aligned_len + usize_len) as *mut MaybeUninit<u8>,
                 len,
             )
-            .copy_from_slice(std::slice::from_raw_parts(
+            .copy_from_slice(slice::from_raw_parts(
                 data as *const T as *const MaybeUninit<u8>,
                 len,
             ));
@@ -299,7 +315,7 @@ impl<T: ?Sized> Deref for List<T> {
 impl<T: ?Sized> Drop for List<T> {
     fn drop(&mut self) {
         if !self.inner.chunks.is_null() {
-            unsafe { std::alloc::dealloc(self.inner.chunks, std::alloc::Layout::from_size_align(self.chunks_cap, self.inner.align).unwrap()) };
+            unsafe { alloc2::dealloc(self.inner.chunks, alloc2::Layout::from_size_align(self.chunks_cap, self.inner.align).unwrap()) };
         }
     }
 }
